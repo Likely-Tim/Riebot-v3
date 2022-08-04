87852904103c4cbfe948ecfd0120fc8c5d6dd4b5
Timothy Wu <timothywu.mail@gmail.com>
2022-08-03 19:15:00 -0700

    Fix Puppeteer Deployment Issue

--- js/arknightsModules.js
+++ js/arknightsModules.js
@@ -7,7 +7,7 @@ const pictures = db.sublevel('pictures');
 const isFinished = db.sublevel('isFinished');
 
 async function refreshArknightsModules() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://gamepress.gg/arknights/database/arknights-module-list', {waitUntil: 'networkidle2'});
     // Get Module List
