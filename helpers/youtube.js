82f385728c1865c30e4d47da8defd1fe623cabd3
Timothy Wu <timothywu.mail@gmail.com>
2022-08-06 20:27:45 -0700

    Add LOCAL_SERVER Support

--- helpers/youtube.js
+++ helpers/youtube.js
@@ -1,23 +1,8 @@
 const fetch = require('node-fetch');
-const Keyv = require('keyv');
-const {KeyvFile} = require('keyv-file');
-const CryptoJS = require('crypto-js');
-
-const PASSWORD = process.env.CRYPTO_PASSWORD;
-
-const tokens = new Keyv({
-  store: new KeyvFile({
-    filename: 'storage/tokens.json',
-    encode: JSON.stringify,
-    decode: JSON.parse,
-  }),
-});
 
 async function search(query) {
   query = encodeURIComponent(query);
-  const apiKeyEncrypted = await tokens.get('youtube_key');
-  const apiKey = CryptoJS.AES.decrypt(apiKeyEncrypted, PASSWORD).toString(CryptoJS.enc.Utf8);
-  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&safeSearch=none&type=video&maxResults=5&order=viewCount&key=${apiKey}`;
+  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&safeSearch=none&type=video&maxResults=5&order=viewCount&key=${process.env.YOUTUBE_KEY}`;
   let response = await fetch(url, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'},
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const fetch = require('node-fetch');
 const crypto = require('crypto');
 
@@ -12,27 +11,39 @@ const SPOTIFY_SECRET = process.env.SPOTIFY_SECRET;
 const MAL_ID = process.env.MAL_ID;
 const MAL_SECRET = process.env.MAL_SECRET;
 
+const LOCAL_SERVER = (process.env.LOCAL_SERVER === 'true');
+
 router.get('/', async (request, response) => {
-    if (request.query.type == "mal") {
-      // When code challenge method is plain, code verifier = code challenge
-      const codeVerifier = generatePKCECodeVerifier();
-      await dbToken.put("malCodeVerifier", codeVerifier);
-      const state = generatePKCECodeVerifier();
-      await dbToken.put("malState", state);
+  if (request.query.type == "mal") {
+    // When code challenge method is plain, code verifier = code challenge
+    const codeVerifier = generatePKCECodeVerifier();
+    await dbToken.put("malCodeVerifier", codeVerifier);
+    const state = generatePKCECodeVerifier();
+    await dbToken.put("malState", state);
+    if (!LOCAL_SERVER) {
       response.redirect(`https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${MAL_ID}&state=${state}&redirect_uri=http://44.242.76.174/auth/mal&code_challenge=${codeVerifier}&code_challenge_method=plain`);
     } else {
-      response.redirect('/');
+      response.redirect(`https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${MAL_ID}&state=${state}&redirect_uri=http://localhost:3000/auth/mal&code_challenge=${codeVerifier}&code_challenge_method=plain`);
     }
+  } else if (request.query.type == "spotify") {
+    if (!LOCAL_SERVER) {
+      response.redirect('https://accounts.spotify.com/en/authorize?client_id=ea2c1c3ca31d409db90f288951542b67&response_type=code&redirect_uri=http:%2F%2F44.242.76.174%2Fauth%2Fspotify&scope=user-read-recently-played%20user-top-read%20user-read-currently-playing%20user-read-playback-state&show_dialog=true');
+    } else {
+      response.redirect('https://accounts.spotify.com/en/authorize?client_id=ea2c1c3ca31d409db90f288951542b67&response_type=code&redirect_uri=http:%2F%2Flocalhost:3000%2Fauth%2Fspotify&scope=user-read-recently-played%20user-top-read%20user-read-currently-playing%20user-read-playback-state&show_dialog=true')
+    }
+  } else {
+    response.redirect('/');
+  }
 })
 
 router.get('/spotify', async (request, response) => {
-    try {
-        await spotifyAccepted(request.query.code);
-        response.redirect('/?spotifySuccess=true');
-    } catch (error) {
-        console.log(error);
-        response.redirect('/?spotifySuccess=false');
-    }
+  try {
+      await spotifyAccepted(request.query.code);
+      response.redirect('/?spotifySuccess=true');
+  } catch (error) {
+      console.log(error);
+      response.redirect('/?spotifySuccess=false');
+  }
 });
 
 router.get('/mal', async (request, response) => {
@@ -51,8 +62,12 @@ router.get('/mal', async (request, response) => {
 
 async function spotifyAccepted(code) {
   const url = 'https://accounts.spotify.com/api/token';
+  let redirect_uri = 'http://44.242.76.174/auth/spotify';
+  if (LOCAL_SERVER) {
+    redirect_uri = "http://localhost:3000/auth/spotify";
+  }
   const authorization = Buffer.from(SPOTIFY_ID + ":" + SPOTIFY_SECRET).toString("base64");
-  const data = {code: code, redirect_uri: 'http://44.242.76.174/auth/spotify', grant_type: 'authorization_code'};
+  const data = {code: code, redirect_uri: redirect_uri, grant_type: 'authorization_code'};
   let response = await fetch(url, {
     method: 'POST',
     headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'Basic ' + authorization},
@@ -69,7 +84,11 @@ async function spotifyAccepted(code) {
 async function malAccepted(code) {
   const url = "https://myanimelist.net/v1/oauth2/token";
   const codeVerifier = await dbToken.get("malCodeVerifier");
-  const data = {client_id: MAL_ID, client_secret: MAL_SECRET, grant_type: "authorization_code", code: code, redirect_uri: "http://44.242.76.174/auth/mal", code_verifier: codeVerifier};
+  let redirect_uri = "http://44.242.76.174/auth/mal";
+  if (LOCAL_SERVER) {
+    redirect_uri = "http://localhost:3000/auth/mal";
+  }
+  const data = {client_id: MAL_ID, client_secret: MAL_SECRET, grant_type: "authorization_code", code: code, redirect_uri: redirect_uri, code_verifier: codeVerifier};
   let response = await fetch(url, {
     method: "POST",
     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
