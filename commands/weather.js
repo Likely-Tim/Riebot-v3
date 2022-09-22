782d9397d73c7ab4f264247aba1e13691260d8a4
Timothy Wu <timothywu.mail@gmail.com>
2022-09-21 19:48:32 -0700

    Visual Crossing Weather Migration

--- commands/weather.js
+++ commands/weather.js
@@ -1,216 +1,58 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
 const fetch = require("node-fetch");
-const { MessageEmbed } = require("discord.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const path = require("path");
+const {MessageEmbed} = require("discord.js");
 
-// Secrets
-const apiKey = process.env.WEATHER_KEY;
+const apiKey = process.env.VISUAL_CROSSING_KEY;
 
-const daysOfTheWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-async function sendGetRequestMain(zip) {
-  const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},US&appid=${apiKey}`;
-  let response = await fetch(url, { method: "GET" });
-  if (response.status == 400 || response.status == 404) {
-    const result = new MessageEmbed().setDescription("Invalid Zip Code");
-    return result;
-  }
+async function sendGetRequest(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?unitGroup=us&key=${apiKey}&contentType=json`;
+  let response = await fetch(url, {method: "GET"});
   response = await response.json();
-  const embed = startEmbed(response);
-  response = await sendGetRequestForecast(
-    response.coord.lat,
-    response.coord.lon
-  );
-  return forecastEmbed(response, embed);
+  return response;
 }
 
-async function sendGetRequestForecast(lat, lon) {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${apiKey}`;
-  const response = await fetch(url, { method: "GET" });
-  return response.json();
+function temperatureConverter(faren) {
+  const cel = ((faren - 32) * 5) / 9;
+  return faren + "°F / " + cel.toFixed(1) + "°C";
 }
 
-function startEmbed(response) {
-  const currentTemp = tempConvert(response.main.temp);
-  const feelsLikeTemp = tempConvert(response.main.feels_like);
-  const result = new MessageEmbed();
-  result.setTitle(
-    `${response.name}, ${titleCap(
-      response.weather[0].description
-    )}, ${titleDate(response.dt, response.timezone)}`
-  );
-  result.setDescription(
-    `**Current Temperature:** ${currentTemp}\n**Feels Like:** ${feelsLikeTemp}\n**Wind:** ${(
-      response.wind.speed * 2.237
-    ).toFixed(1)} mph ${windDirection(response.wind.deg)}\n**Humidity:** ${
-      response.main.humidity
-    }%\n**Sunrise:** ${timeReturn(
-      response.sys.sunrise,
-      response.timezone
-    )}\n**Sunset:** ${timeReturn(response.sys.sunset, response.timezone)}`
-  );
-  result.setThumbnail(
-    `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
-  );
-  return result;
-}
+async function buildEmbed(data) {
+  const currentConditions = data.currentConditions;
+  const embed = new MessageEmbed();
+  const currentDate = new Date(currentConditions.datetimeEpoch * 1000);
+  embed.setThumbnail("attachment://weather.png");
+  embed.setTitle(`${data.resolvedAddress}, ${currentDate.toLocaleDateString("en-US", {timeZone: data.timezone, month: "2-digit", day: "2-digit", hour: "numeric", minute: "numeric"})}\n${currentConditions.conditions}`);
+  embed.setDescription(`**${data.description}**\n\n**Current Temperature:** ${temperatureConverter(currentConditions.temp)}\n**Feels Like:** ${temperatureConverter(currentConditions.feelslike)}\n**Humidity:** ${currentConditions.humidity}%\n**Wind:** ${currentConditions.windspeed} mph\n**Sunrise:** ${currentConditions.sunrise}\n **Sunset:** ${currentConditions.sunset}\n**UV Index:** ${currentConditions.uvindex}`);
+  const forecast = data.days;
+  for (let i = 1; i < forecast.length && i < 7; i++) {
+    const date = new Date(forecast[i].datetimeEpoch * 1000);
+    embed.addField(date.toLocaleDateString("en-US", {timeZone: data.timezone, weekday: "long"}), `**Min:** ${temperatureConverter(forecast[i].tempmin)}\n**Max** ${temperatureConverter(forecast[i].tempmax)}\n**Percipitation:** ${forecast[i].precipprob}%`, true);
+  }
+  const alerts = data.alerts;
 
-function forecastEmbed(response, result) {
-  let alerts = "";
-  if (typeof response.alerts === "undefined") {
-    alerts = "None";
-  } else {
-    for (let i = 0; i < response.alerts.length; i++) {
-      alerts +=
-        response.alerts[i].sender_name +
-        ": " +
-        response.alerts[i].event +
-        " (" +
-        dateConvert(response.alerts[i].start, response.timezone_offset) +
-        " - " +
-        dateConvert(response.alerts[i].end, response.timezone_offset) +
-        ")\n";
+  if (alerts.length != 0) {
+    const alertArray = [];
+    for (let i = 0; i < alerts.length; i++) {
+      const startTime = new Date(alerts[i].onsetEpoch * 1000);
+      const endTime = new Date(alerts[i].endsEpoch * 1000);
+      alertArray.push(`[[${startTime.toLocaleDateString("en-US", {timeZone: data.timezone, month: "2-digit", day: "2-digit"}) + " " + startTime.toLocaleTimeString("en-US", {timeZone: data.timezone, hour12: false, hour: "2-digit", minute: "2-digit"})} - ${endTime.toLocaleDateString("en-US", {timeZone: data.timezone, month: "2-digit", day: "2-digit"}) + " " + endTime.toLocaleTimeString("en-US", {timeZone: data.timezone, hour12: false, hour: "2-digit", minute: "2-digit"})}] ${alerts[i].event}](${alerts[i].link})`);
     }
+    embed.addField("Alerts", alertArray.join("\n"), false);
   }
-  const day = new Date(
-    (response.daily[1].dt + response.timezone_offset) * 1000
-  ).getDay();
-  result.addFields(
-    {
-      name: daysOfTheWeek[day % 7],
-      value: `**Min:** ${tempConvert(
-        response.daily[1].temp.min
-      )}\n**Max:** ${tempConvert(response.daily[1].temp.max)}`,
-      inline: true,
-    },
-    {
-      name: daysOfTheWeek[(day + 1) % 7],
-      value: `**Min:** ${tempConvert(
-        response.daily[2].temp.min
-      )}\n**Max:** ${tempConvert(response.daily[2].temp.max)}`,
-      inline: true,
-    },
-    {
-      name: daysOfTheWeek[(day + 2) % 7],
-      value: `**Min:** ${tempConvert(
-        response.daily[3].temp.min
-      )}\n**Max:** ${tempConvert(response.daily[3].temp.max)}`,
-      inline: true,
-    },
-    {
-      name: daysOfTheWeek[(day + 3) % 7],
-      value: `**Min:** ${tempConvert(
-        response.daily[4].temp.min
-      )}\n**Max:** ${tempConvert(response.daily[4].temp.max)}`,
-      inline: true,
-    },
-    {
-      name: daysOfTheWeek[(day + 4) % 7],
-      value: `**Min:** ${tempConvert(
-        response.daily[5].temp.min
-      )}\n**Max:** ${tempConvert(response.daily[5].temp.max)}`,
-      inline: true,
-    },
-    {
-      name: "Alerts",
-      value: `${alerts}`,
-    }
-  );
-
-  result.setColor("#4287f5");
-  return result;
-}
-
-function timeReturn(sec, offset) {
-  const time = new Date((sec + offset) * 1000);
-  const minutes = "0" + time.getMinutes();
-  return time.getHours() + ":" + minutes.substr(-2);
-}
-
-function tempConvert(temp) {
-  const cel = temp - 273.15;
-  const faren = (cel * 9) / 5 + 32;
-  return faren.toFixed(1) + "°F / " + cel.toFixed(1) + "°C";
-}
-
-function windDirection(degree) {
-  switch (true) {
-    case degree < 12:
-      return "N";
-    case degree < 34:
-      return "NNE";
-    case degree < 57:
-      return "NE";
-    case degree < 79:
-      return "ENE";
-    case degree < 102:
-      return "E";
-    case degree < 124:
-      return "ESE";
-    case degree < 147:
-      return "SE";
-    case degree < 169:
-      return "SSE";
-    case degree < 192:
-      return "S";
-    case degree < 214:
-      return "SSW";
-    case degree < 237:
-      return "SW";
-    case degree < 259:
-      return "WSW";
-    case degree < 282:
-      return "W";
-    case degree < 304:
-      return "WNW";
-    case degree < 327:
-      return "NW";
-    case degree < 349:
-      return "NNW";
-    default:
-      return "N";
-  }
-}
-
-function titleCap(string) {
-  const sentence = string.toLowerCase().split(" ");
-  for (let i = 0; i < sentence.length; i++) {
-    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-  }
-  return sentence.join(" ");
-}
-
-function titleDate(sec, offset) {
-  const time = timeReturn(sec, offset);
-  let date = new Date((sec + offset) * 1000);
-  date = String(Number(date.getMonth()) + 1) + "/" + date.getDate();
-  return date + " " + time;
-}
-
-function dateConvert(sec, offset) {
-  const date = new Date((sec + offset) * 1000);
-  return String(Number(date.getMonth()) + 1) + "/" + date.getDate();
+  return embed;
 }
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("weather")
     .setDescription("What is the weather?")
-    .addIntegerOption((option) =>
-      option.setName("zip").setDescription("Enter zip code").setRequired(true)
-    ),
-
+    .addStringOption((option) => option.setName("location").setDescription("Where").setRequired(true)),
   async execute(client, interaction) {
-    const zip = interaction.options.getInteger("zip");
-    const embed = await sendGetRequestMain(zip);
-    interaction.reply({ embeds: [embed] });
-    return zip;
+    const location = interaction.options.getString("location");
+    const data = await sendGetRequest(location);
+    const embed = await buildEmbed(data);
+    interaction.reply({embeds: [embed], files: [{attachment: path.join(__dirname, `../media/VisualCrossingIcons/${data.currentConditions.icon}.png`), name: "weather.png"}]});
+    return "N/A";
   },
 };
