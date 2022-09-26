614ec0dab0315784ec7f0e6c133495f7759cc728
Timothy Wu <timothywu.mail@gmail.com>
2022-09-25 20:08:25 -0700

    Tesseract Error Handling

--- js/tesseract-ocr.js
+++ js/tesseract-ocr.js
@@ -1,15 +1,27 @@
 const tesseract = require("node-tesseract-ocr");
 const dbTesseract = require("../databaseUtils/tesseract");
+const {logger} = require("../utils/logger");
 
 const config = {
   lang: "eng",
 };
 
+function checkURL(url) {
+  return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
+}
+
 async function tesseractExtractText(messageAttachmentArray) {
   for (let i = 0; i < messageAttachmentArray.length; i++) {
-    const url = messageAttachmentArray[i].url;
-    const extractedText = await tesseract.recognize(url, config);
-    dbTesseract.put(extractedText, url);
+    try {
+      const url = messageAttachmentArray[i].url;
+      if (checkURL(url)) {
+        const extractedText = await tesseract.recognize(url, config);
+        dbTesseract.put(extractedText, url);
+        console.log(extractedText);
+      }
+    } catch (error) {
+      logger.info(`[Tesseract] Text Extraction Fail`);
+    }
   }
 }
 
